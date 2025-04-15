1fac646d6e7a74c5dc461f51ab35bdd28ac43074
Galina A. Basargina <gala.basargina@yandex.ru>
2025-04-15 14:15:50 +0300

    Выполнены дополнительные задания

--- pages/contact.php
+++ pages/contact.php
@@ -1,10 +1,12 @@
 <?php include '../includes/header.php'; ?>
+<link rel="stylesheet" href="/assets/css/style.css">
+
 <main>
     <h1>Контакты</h1>
-    <form method="post">
-        <input type="text" name="name" placeholder="Имя">
-        <input type="email" name="email" placeholder="Email">
-        <textarea name="message"></textarea>
+    <form method="post" action="contact_form.php">
+        <input type="text" name="name" placeholder="Имя" required minlenght="2">
+        <input type="email" name="email" placeholder="Email" required>
+        <textarea name="message" minlength="10"></textarea>
         <button type="submit">Отправить</button>
     </form>
 </main>
